c655cb9396a8c6bb94e40061345c30870618e2d6
Rinaldo <rinaldobadigar98@gmail.com>
2023-01-10 11:41:26 +0530

    Redux

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/NavBar'
+import Products from '../components/Products'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Products/>
+        </div>
+    )
+}
+
+export default Home
